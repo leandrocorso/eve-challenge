3d97b2339bcaa27ac7de3e0733debc92b7fea765
Leandro <contato@leandrocorso.com.br>
2026-03-25 15:04:46 -0300

    Creating a basic provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+
+const queryClient = new QueryClient();
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
 function App() {
-  return <></>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter></BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
