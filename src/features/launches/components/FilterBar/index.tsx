4acf3e7c9fa89bbb822c36e057d6615ff4ae5800
Leandro <contato@leandrocorso.com.br>
2026-03-26 00:21:02 -0300

    Updating FilterBar UI

--- src/features/launches/components/FilterBar/index.tsx
+++ src/features/launches/components/FilterBar/index.tsx
@@ -5,9 +5,10 @@ import {
   FormControl,
   InputLabel,
   Select,
-  Stack,
   type SelectChangeEvent,
+  Grid,
 } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
 
 import type { FiltersBarProps } from "./FilterBarProps";
 
@@ -18,6 +19,10 @@ export const FiltersBar = ({
   onStatusChange,
   upcoming,
   onUpcomingChange,
+  dateFrom,
+  onDateFromChange,
+  dateTo,
+  onDateToChange,
 }: FiltersBarProps) => {
   return (
     <Box
@@ -29,44 +34,69 @@ export const FiltersBar = ({
         boxShadow: 1,
       }}
     >
-      <Stack direction={{ xs: "column", md: "row" }} spacing={2}>
-        <TextField
-          fullWidth
-          label="Buscar por nome"
-          variant="outlined"
-          value={search}
-          onChange={(e) => onSearchChange(e.target.value)}
-          placeholder="Ex: Falcon, Starlink..."
-        />
+      <Grid container spacing={2} alignItems="center">
+        <Grid size={{ xs: 12, md: 4 }}>
+          <TextField
+            fullWidth
+            label="Buscar por nome"
+            variant="outlined"
+            value={search}
+            onChange={(e) => onSearchChange(e.target.value)}
+            placeholder="Ex: Falcon, Starlink..."
+          />
+        </Grid>
 
-        <FormControl fullWidth>
-          <InputLabel>Sucesso</InputLabel>
-          <Select
-            value={status}
-            label="Sucesso"
-            onChange={(e: SelectChangeEvent) => onStatusChange(e.target.value)}
-          >
-            <MenuItem value="all">Todos</MenuItem>
-            <MenuItem value="success">Sucesso</MenuItem>
-            <MenuItem value="failed">Falha</MenuItem>
-          </Select>
-        </FormControl>
+        <Grid size={{ xs: 12, sm: 6, md: 2 }}>
+          <FormControl fullWidth>
+            <InputLabel>Status</InputLabel>
+            <Select
+              value={status}
+              label="Sucesso"
+              onChange={(e: SelectChangeEvent) =>
+                onStatusChange(e.target.value)
+              }
+            >
+              <MenuItem value="all">Todos</MenuItem>
+              <MenuItem value="success">Sucesso</MenuItem>
+              <MenuItem value="failed">Falha</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
 
-        <FormControl fullWidth>
-          <InputLabel>Lançamentos</InputLabel>
-          <Select
-            value={upcoming}
-            label="Tipo"
-            onChange={(e: SelectChangeEvent) =>
-              onUpcomingChange(e.target.value)
-            }
-          >
-            <MenuItem value="all">Todos</MenuItem>
-            <MenuItem value="upcoming">Próximos</MenuItem>
-            <MenuItem value="past">Passados</MenuItem>
-          </Select>
-        </FormControl>
-      </Stack>
+        <Grid size={{ xs: 12, sm: 6, md: 2 }}>
+          <FormControl fullWidth>
+            <InputLabel>Lançamentos</InputLabel>
+            <Select
+              value={upcoming}
+              label="Tipo"
+              onChange={(e: SelectChangeEvent) =>
+                onUpcomingChange(e.target.value)
+              }
+            >
+              <MenuItem value="all">Todos</MenuItem>
+              <MenuItem value="upcoming">Próximos</MenuItem>
+              <MenuItem value="past">Passados</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid size={{ xs: 12, sm: 6, md: 2 }}>
+          <DatePicker
+            label="Data início"
+            value={dateFrom}
+            onChange={onDateFromChange}
+            slotProps={{ textField: { fullWidth: true } }}
+          />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 2 }}>
+          <DatePicker
+            label="Data fim"
+            value={dateTo}
+            onChange={onDateToChange}
+            slotProps={{ textField: { fullWidth: true } }}
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
