a2e0fff3f14e28425539f43763d0369be9015bf2
Leandro <contato@leandrocorso.com.br>
2026-03-27 13:01:45 -0300

    Adding locale into test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,10 @@
 import "@testing-library/jest-dom/vitest";
+import dayjs from "dayjs";
+
 import { server } from "./server";
+import "dayjs/locale/pt-br";
+
+dayjs.locale("pt-br");
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
